5ca01c128e4e3975ba9cd31a8fbbe1dee582dacb
Finleth <jeffrey@paufamily.com>
2017-12-22 14:11:19 -0800

    Finished API setup

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
 const express = require('express');
+const cors = require('cors');
 const PORT = process.env.PORT || 9000;
 
 const server = express();
+
+server.use(cors());
+
+
+server.get('/api/get-stuff', (req, res) => {
+    const stuff = {
+        name: 'Stuffins',
+        message: 'Here is some stuff'
+    };
+
+    res.send(stuff);
+});
+
+server.get('/api/better-stuff', (req, res) => {
+    const betterStuff = {
+        name: 'Better Stuff',
+        message: 'This is the real deal'
+    };
+
+    res.send(betterStuff);
+});
 
 
 server.get('*', (req, res) => {
